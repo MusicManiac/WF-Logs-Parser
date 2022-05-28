a57deda3664f396b23cea5185d2109d5344b52c3
MusicManiac <14209235+MusicManiac@users.noreply.github.com>
2022-05-29 01:32:01 +0200

    Finished parser Added base xlsx to display stuff

--- include/tradeParser.h
+++ include/tradeParser.h
@@ -3,29 +3,60 @@
 
 using namespace ::std;
 
-const string ITEMS_RECIEVED_STRING = "	TRADED ITEMS RECIEVED :";
+const string ITEMS_RECEIVED_STRING = "	TRADED ITEMS RECIEVED :";
 
 struct tradeInfo {
 	string timeStamp;
-	int tradeType; // 1 - buying, 2 - selling, 3 - exchange
+	int tradeType = 3; // 1 - buying, 2 - selling, 3 - exchange
 	string offeredItems[6] = {"-","-","-","-","-","-"}; // plain number means plat paid
 	string receivedItems[6] = {"-","-","-","-","-","-"}; // plain number means plat received
-	int creditsSpent;
+	string creditsSpent;
 };
 
 string tradeIntoCSV (vector<string> rawTrade) {
     tradeInfo trade;
     trade.timeStamp = rawTrade[0].substr(0, 19); // date and time are always 19 char long
-    for (int i = 2; i < rawTrade.size(); i++){ // strat from 2, becasue 0 is date and 1 is "	TRADED ITEMS GIVEN :"
+    int currentOfferedItemsIndex = 0, currentReceivedItemsIndex = 0;
+    bool processingReceivedItems = false;
+    for (int i = 2; i < rawTrade.size(); i++){ // start from 2, because 0 is date and 1 is "	TRADED ITEMS GIVEN :"
         string currentString = rawTrade[i];
-        if (currentString.substr(2,8) == "PLATINUM") { // process plat line
-            trade.tradeType = 1;
-            trade.offeredItems[0] = currentString.substr(currentString.find("-"));
-            continue; 
-        }
-        if (currentString.substr(2,7) == "CREDITS") { // process credit line
-            trade.creditsSpent = stoi(currentString.substr(currentString.find("-")));
-            continue;
+        if (!processingReceivedItems) {
+            if (currentString.substr(2,8) == "PLATINUM") { // process plat line
+                trade.tradeType = 1;
+                trade.offeredItems[currentOfferedItemsIndex++] = currentString.substr(currentString.find("-"));
+                continue; 
+            }
+            if (currentString.substr(2,7) == "CREDITS") { // process credit line
+                trade.creditsSpent = currentString.substr(currentString.find("-"));
+                continue;
+            }
+            if (currentString != ITEMS_RECEIVED_STRING) { // keep processing lines until "	TRADED ITEMS RECIEVED :"is found
+                if (currentString.find(":") != string::npos) { // if item has count, add items to array as many times as amount of copies offered, max 6 so length is 1
+                    for (int i = 0; i < stoi(currentString.substr(currentString.size() - 1)); i++) {
+                        trade.offeredItems[currentOfferedItemsIndex++] = currentString.substr(2, currentString.size() - 2 - 5); // -2 for tabs and -5 for amount of items traded " : -X"
+                    }
+                } else { // else it's some stuff like "MELEE RIVEN MOD" or "VAYOR HEK"
+                    trade.offeredItems[currentOfferedItemsIndex++] = currentString.substr(2); // remove 2 tabs at start
+                }
+            } else {
+                processingReceivedItems = true;
+                continue;
+            }
+        } else {
+            if (currentString.substr(2,8) == "PLATINUM") { // process plat line
+                trade.tradeType = 2;
+                trade.receivedItems[currentReceivedItemsIndex++] = currentString.substr(currentString.find(":") + 2);
+                continue; 
+            }
+            if (currentString.find(":") != string::npos) { // if item has count, add items to array as many times as amount of copies offered, max 6 so length is 1
+                for (int i = 0; i < stoi(currentString.substr(currentString.size() - 1)); i++) {
+                    trade.receivedItems[currentReceivedItemsIndex++] = currentString.substr(2, currentString.size() - 2 - 4); // -2 for tabs and -4 for amount of items traded " : X"
+                }
+            } else { // else it's some stuff like "MELEE RIVEN MOD" or "VAYOR HEK"
+                trade.receivedItems[currentReceivedItemsIndex++] = currentString.substr(2); // remove 2 tabs at start
+
+            }
+            
         }
         
     }
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <tradeParser.h>
 
 using namespace ::std;
 
@@ -9,19 +10,27 @@ const string TRADE_BEGINNING = "	TRADED ITEMS GIVEN :";
 
 
 int main(void) {
-	string filename = "test.txt";
 
-	//cout << "Filename:";
-	//cin >> filename;
+	string filename = "";
+
+	cout << "Filename (with extension, looks like \"numbers_yourNickname_GDPR_Trades_Warframe.txt\"):";
+	cin >> filename;
 
 	fstream logFile;
+	ofstream csvFile;
 
-	vector<string> wholeTrade;
+	csvFile.open("ParsedTrades.csv", std::ofstream::out | std::ofstream::trunc); // erase current csv file if present
+	csvFile.close();
 
-	logFile.open(filename , ios::in);
-	if (logFile.is_open()) {
+	logFile.open(filename, ios::in);
+	csvFile.open("ParsedTrades.csv", ios::app);
+	csvFile << "Date,Offered Item 1,Offered Item 2,Offered Item 3,Offered Item 4,Offered Item 5,Offered Item 6,Received Item 1,Received Item 2,Received Item 3,Received Item 4,Received Item 5,Received Item 6,Creds Spent,Trade Type\n"; // header for csv
+
+	if (logFile.is_open() && csvFile.is_open())
+	{
 		string freshLine = "", oldLine = "";
 		bool processingTrade = false;
+		vector<string> wholeTrade;
 		while (getline(logFile, freshLine)) {
 			if (freshLine == TRADE_BEGINNING) { // save previous (date) and current line to vector, since we know it's a trade
 				wholeTrade.push_back(oldLine);
@@ -30,15 +39,22 @@ int main(void) {
 				continue;
 			}
 			if(processingTrade) { // keep pushing lines into vector until we find empty line
-				wholeTrade.push_back(freshLine);
-				if (freshLine.empty()){
-					processingTrade = false; // uncheck the flag once empy line is found
+				if (freshLine.empty() || logFile.eof()){
+					processingTrade = false; // uncheck the flag once empty line is found
+					if (logFile.eof()) {
+						wholeTrade.push_back(freshLine);
+					}
+					csvFile << tradeIntoCSV(wholeTrade);
+					wholeTrade.clear();
+					continue;
 				}
+				wholeTrade.push_back(freshLine);
 			}
 			oldLine = freshLine; // save previous line, since we won't know we're in a trade until we find "	TRADED ITEMS GIVEN :" line
 		}
 		logFile.close();
-	} else {
+	}
+	else {
 		cout << "Didn't get the file";
 	}
 	return 0;
